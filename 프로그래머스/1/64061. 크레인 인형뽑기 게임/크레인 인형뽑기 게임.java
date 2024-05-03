8f82131a7493eee52f3794c083e3c793978e3eaf
박창대 <ckdeo070@naver.com>
2024-05-03 15:02:44 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.80 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Solution {
+    public int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+        
+        for(int move : moves) {
+            for (int i = 0; i < board.length; i++) {
+                int doll = board[i][move - 1];
+                if (doll != 0) {
+                    if (!stack.isEmpty() && stack.peek() == doll) {
+                        stack.pop();
+                        answer += 2;
+                    } else {
+                        stack.push(doll);
+                    }
+
+                    board[i][move - 1] = 0;
+                    break;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
